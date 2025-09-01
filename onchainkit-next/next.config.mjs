cd4da4c55ef8b3acaafd6ba31524d2bb8f881916
shawt3000 <brilliantfiretech@gmail.com>
2025-09-01 01:54:42 -0400

    chore: 307 redirect /.well-known/farcaster.json to hosted manifest

--- onchainkit-next/next.config.mjs
+++ onchainkit-next/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination:
+          'https://api.farcaster.xyz/miniapps/hosted-manifest/019903d3-5094-29ad-b589-71f0fb1a1e83',
+        // permanent false => 307 Temporary Redirect
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
